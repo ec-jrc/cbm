59fb6c29b65959273df5026a91dd793210fa4f1c
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-02-15 09:17:20 +0000

    Reconfigured start up checks

--- cbm/__init__.py
+++ cbm/__init__.py
@@ -13,3 +13,25 @@ __version__ = "01/2021"
 __maintainer__ = ["Guido Lemoine", "Konstantinos Anastasakis"]
 __email__ = ""
 __status__ = "Development"
+
+import os
+import sys
+from os.path import dirname, abspath, join
+from cbm.utils import config
+
+if sys.version_info < (3, 6):
+    print("Not supoted python version, cbm needs python version > 3.6")
+    sys.exit()
+
+paths = config.get_value(['paths'])
+paths['config'] = 'config'
+
+for p in paths:
+    try:
+        if not os.path.exists(join(config.folder_repo, paths[p])):
+            os.makedirs(join(config.folder_repo, paths[p]))
+            print(f"Directory {p} created successfully")
+    except OSError as error:
+        print(f"Directory {p} can not be created")
+
+config.create()
