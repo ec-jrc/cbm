1b031957b1ea983e71f46b14fdc76680ac708dd4
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-02-18 05:39:13 +0000

    Fix lowercase usernames bug and add delete user function in RESTful API users module

--- cbm/api/users.py
+++ cbm/api/users.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # This file is part of CbM (https://github.com/ec-jrc/cbm).
-# Author    : Guido Lemoine, Konstantinos Anastasakis
+# Author    : Konstantinos Anastasakis
 # Credits   : GTCAP Team
 # Copyright : 2021 European Commission, Joint Research Centre
 # License   : 3-Clause BSD
@@ -14,6 +14,7 @@ import hashlib
 import json
 import os
 
+users_file = '_users.json'
 
 def auth(username, password):
     """Authentication check with hashed passwords"""
@@ -51,27 +52,44 @@ def add(username, password=''):
         users = {}
 
     salt = os.urandom(32)
-
     key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-    users[username] = {
+    users[username.lower()] = {
         'salt': str(salt)[2:-1],
         'key': str(key)[2:-1]
     }
-    with open('_users.json', 'w') as users_file:
-        json.dump(users, users_file, indent=2)
+    with open(users_file, 'w') as u:
+        json.dump(users, u, indent=2)
     print(f"The user '{username}' was added.")
 
 
 def get_users():
     """Get the list of active users"""
     try:
-        with open('_users.json', 'r') as users_file:
-            users = json.load(users_file)
-#         print(f"Users list: {users.keys()}")
+        with open(users_file, 'r') as u:
+            users = json.load(u)
         return users
     except Exception:
         try:
-            with open('_users.json', 'w') as users_file:
+            with open(users_file, 'w') as u:
                 users = json.dumps({}, sort_keys=False, indent=2)
+            return users
         except Exception as err:
             return err
+
+
+def delete(username):
+    """Delete user from users_file file.
+
+    Example:
+        config.delete('MyUserName')
+
+    """
+    users = get_users()
+
+    for user in list(users):
+        if user == username:
+            del users[username]
+            print(f"The user '{user}' is deleted.")
+
+    with open(users_file, 'w') as u:
+        json.dump(users, u, indent=2)
