49f3eb487bc6b764703fae1e175c7f71bc2abf48
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-02-19 16:31:43 +0000

    New option to get background by parcel ID

--- cbm/view/background.py
+++ cbm/view/background.py
@@ -68,6 +68,56 @@ def by_location(aoi, year, lon, lat, chipsize=512, extend=512, tms='Google', pre
         else:
             plt.show()
 
+def by_pid(aoi, year, pid, chipsize=512, extend=512, tms='Google', prefix='', folder='temp'):
+    """Download the background image with parcels polygon overlay by selected location.
+
+    Examples:
+        from cbm.view import background
+        background.by_location(aoi, year, lon, lat, 512, 512, 'Google', 'temp/test.tif', True)
+
+    Arguments:
+        aoi, the area of interest e.g.: es, nld (str)
+        year, the year of the parcels dataset (int)
+        pid, the parcel id (str).
+        chipsize, size of the chip in pixels (int).
+        extend, size of the chip in meters  (float).
+        tms, tile map server Google or Bing (str).
+        prefix, the name of the output file (str).
+        folder, the folder to be stored (str).
+        quiet, print or not procedure information (Boolean).
+
+    """
+    json_data = json.loads(api.pid(aoi, year, pid, True))
+    with open(f'{folder}{prefix}info.json', "w") as f:
+        json.dump(json_data, f)
+    
+    lon, lat = spatial_utils.centroid(json.loads(json_data['geom'][0])['coordinates'][0])
+    background.main(lon, lat, chipsize, extend, tms, f'{folder}{tms}.tif')
+
+    with rasterio.open(f'{folder}/{prefix}{tms}.tif') as img:
+        def overlay_parcel(img, json_data):
+            img_epsg = img.crs.to_epsg()
+            geo_json = spatial_utils.transform_geometry(
+                json_data, img_epsg)
+            patche = [PolygonPatch(feature, edgecolor="yellow",
+                                   facecolor="none", linewidth=2
+                                   ) for feature in [geo_json]]
+            return patche
+
+        ax = plt.gca()
+        for p in overlay_parcel(img, json_data):
+            ax.add_patch(p)
+
+        show(img, ax=ax)
+
+        if prefix:
+            plt.savefig(f'{folder}/{prefix}{tms}.png', dpi=None, facecolor='w', edgecolor='w',
+                    orientation='portrait', format=None,
+                    transparent=False, bbox_inches=None, pad_inches=0.1,
+                    metadata=None)
+        else:
+            plt.show()
+
 
 if __name__ == "__main__":
     import sys
