d7d15f11700d88ae23abd41ab55b9e02f4ab86c2
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-02-23 13:29:22 +0000

    New function to view backgrounds with a slider widget for ipycbm

---
+++ cbm/ipycbm/ipy_view/background.py
@@ -0,0 +1,72 @@
+import os
+import os.path
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from ipywidgets import Output, VBox, SelectionSlider
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+from cbm.utils import config
+from cbm import get
+
+def slider(aoi, year, pid, chipsize=512, extend=512, tms=['Google']):
+
+    workdir = config.get_value(['paths', 'temp'])
+    path = f'{workdir}/{aoi}{year}/pid{pid}/background/'
+
+    for t in tms:
+        if not os.path.isfile(f'{path}{t.lower()}.png'):
+            get.background.by_pid(aoi, year, pid, chipsize, extend, t, True)
+        
+    selection = SelectionSlider(
+        options=tms,
+        value=tms[0],
+        disabled=False,
+        continuous_update=False,
+        orientation='horizontal',
+        readout=True
+    )
+    output = Output()
+
+    with output:
+        img = mpimg.imread(f'{path}{selection.value.lower()}.png')
+        plt.imshow(img)
+        plt.axis('off')
+        plt.show()
+
+    def on_value_change(change):
+        with output:
+            output.clear_output()
+            img = mpimg.imread(f'{path}{selection.value.lower()}.png')
+            plt.imshow(img)
+            plt.axis('off')
+            plt.show()
+
+    selection.observe(on_value_change, names='value')
+    return VBox([selection, output])
+
+
+def grid(aoi, year, pid, chipsize=512, extend=512, tms='Google'):
+
+    workdir = config.get_value(['paths', 'temp'])
+    path = f'{workdir}/{aoi}{year}/pid{pid}/background/'
+
+    for t in tms:
+        if not os.path.isfile(f'{path}{t.lower()}.png'):
+            get.background.by_pid(aoi, year, pid, chipsize, extend, t, True)
+
+    columns = 5
+    rows = int(len(tms) // columns + (len(tms) % columns > 0))
+    fig = plt.figure(figsize=(25, 5 * rows))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                     nrows_ncols=(rows, columns),  # creates 2x2 grid of axes
+                     axes_pad=0.1,  # pad between axes in inch.
+                     )
+
+    for ax, im in zip(grid, tms):
+        # Iterating over the grid returns the Axes.
+        ax.axis('off')
+        ax.imshow(plt.imread(f'{path}{im.lower()}.png', 3))
+        ax.set_title(im)
+
+    plt.show()
+
