7b32dd1da0a3a98678cc5db7be18d4d69aa0929e
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-03-18 16:41:09 +0000

    Improve path handling

--- cbm/get/background.py
+++ cbm/get/background.py
@@ -11,7 +11,7 @@ import json
 from os.path import join, normpath, exists, isfile
 
 from cbm.sources import api
-from cbm.utils import spatial, config
+from cbm.utils import spatial_utils, config
 
 
 def by_location(aoi, year, lon, lat, chipsize=512, extend=512,
@@ -33,17 +33,18 @@ def by_location(aoi, year, lon, lat, chipsize=512, extend=512,
         tms, tile map server Google or Bing (str).
         quiet, print or not procedure information (Boolean).
     """
+
     json_data = json.loads(api.ploc(aoi, year, lon, lat, True))
     if type(json_data['ogc_fid']) is list:
         pid = json_data['ogc_fid'][0]
     else:
         pid = json_data['ogc_fid']
 
-    workdir = config.get_value(['paths', 'temp'])
-    path = normpath(join(f'{workdir}{aoi}{year}', pid))
-    json_file = normpath(join(path, 'info.json'))
-    if not exists(path):
-        os.makedirs(path)
+    workdir = normpath(join(config.get_value(['paths', 'temp']),
+                            f'{aoi}{str(year)}', str(pid)))
+    json_file = normpath(join(workdir, 'info.json'))
+    if not exists(workdir):
+        os.makedirs(workdir)
     if not isfile(json_file):
         with open(json_file, "w") as f:
             json.dump(json_data, f)
@@ -69,15 +70,15 @@ def by_pid(aoi, year, pid, chipsize=512, extend=512,
         extend, size of the chip in meters  (float).
         tms, tile map server Google or Bing (str).
         quiet, print or not procedure information (Boolean).
-
     """
-    workdir = config.get_value(['paths', 'temp'])
-    path = normpath(join(f'{workdir}{aoi}{year}', pid))
-    json_file = normpath(join(path, 'info.json'))
+
+    workdir = normpath(join(config.get_value(['paths', 'temp']),
+                            f'{aoi}{str(year)}', str(pid)))
+    json_file = normpath(join(workdir, 'info.json'))
     if not isfile(json_file):
         json_data = json.loads(api.pid(aoi, year, pid, True))
-        if not exists(path):
-            os.makedirs(path)
+        if not exists(workdir):
+            os.makedirs(workdir)
 
         with open(json_file, "w") as f:
             json.dump(json_data, f)
@@ -85,8 +86,8 @@ def by_pid(aoi, year, pid, chipsize=512, extend=512,
         with open(json_file, 'r') as f:
             json_data = json.load(f)
 
-    lat, lon = spatial.centroid(
-        spatial.transform_geometry(json_data))
+    lat, lon = spatial_utils.centroid(
+        spatial_utils.transform_geometry(json_data))
 
     api.background(lon, lat, chipsize, extend,
                    tms, aoi, year, pid, quiet)
