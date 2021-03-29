dcc1b4dd4cf7c3d24a3c8ddc72d381746c412087
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-03-29 20:58:10 +0000

    Update background viewing module

--- cbm/get/chip_images.py
+++ cbm/get/chip_images.py
@@ -13,7 +13,70 @@ from os.path import join, normpath, isfile, dirname
 from cbm.utils import config
 
 
-def by_pid(aoi, year, pid, start_date, end_date, band, chipsize, quiet=False):
+def by_location(aoi, year, lon, lat, start_date, end_date, band, chipsize,
+                quiet=True):
+    """Download the chip image by selected location.
+
+    Examples:
+        import cbm
+        cbm.get.chip_images.by_pid(aoi, year, pid, start_date, end_date,
+                                    band, chipsize)
+
+    Arguments:
+        aoi, the area of interest e.g.: es, nld (str)
+        year, the year of the parcels dataset (int)
+        lat, lon, the the coords of the parcel (float).
+        start_date, Start date '2019-06-01' (str)
+        end_date, End date '2019-06-01' (str)
+        band, 3 Sentinel-2 band names. One of [‘B02’, ‘B03’, ‘B04’, ‘B08’]
+            (10 m bands) or [‘B05’, ‘B06’, ‘B07’, ‘B8A’, ‘B11’, ‘B12’]
+            (20 m bands). 10m and 20m bands can be combined.
+            The first band determines the resolution in the output
+            composite. Defaults to B08_B04_B03.
+        chipsize, size of the chip in pixels (int).
+    """
+    get_requests = data_source()
+    json_data = json.loads(get_requests.ploc(aoi, year, lon, lat, True))
+    if type(json_data['ogc_fid']) is list:
+        pid = json_data['ogc_fid'][0]
+    else:
+        pid = json_data['ogc_fid']
+
+    workdir = normpath(join(config.get_value(['paths', 'temp']),
+                            f'{aoi}{str(year)}', str(pid)))
+    pfile = normpath(join(workdir, 'info.json'))
+
+    if not isfile(pfile):
+        parcel = json.loads(get_requests.pid(aoi, year, pid, True))
+        try:
+            os.makedirs(dirname(pfile), exist_ok=True)
+            with open(pfile, "w") as f:
+                json.dump(parcel, f)
+            if not quiet:
+                print(f"File saved at: {pfile}")
+        except Exception as err:
+            print(f"Could not create the file: {err}")
+    else:
+        with open(pfile, "r") as f:
+            parcel = json.load(f)
+    images_dir = normpath(join(workdir, 'chip_images'))
+    if not quiet:
+        print(f"Getting '{band}' chip images for parcel: {pid}")
+
+    get_requests.rcbl(parcel, start_date, end_date, [band],
+                      chipsize, images_dir)
+
+    images_list = normpath(join(workdir, 'chip_images',
+                                f'images_list.{band}.csv'))
+    if file_len(images_list) > 1:
+        if not quiet:
+            print(f"Completed, all GeoTIFFs for band '{band}' are downloaded",
+                  f" in the folder: '{workdir}/chip_images'")
+    else:
+        print("No files where downloaded, please check your configurations")
+
+
+def by_pid(aoi, year, pid, start_date, end_date, band, chipsize, quiet=True):
     """Download the chip image by selected parcel id.
 
     Examples:
@@ -55,7 +118,6 @@ def by_pid(aoi, year, pid, start_date, end_date, band, chipsize, quiet=False):
     if not quiet:
         print(f"Getting '{band}' chip images for parcel: {pid}")
 
-    # parcel = json.loads(get_requests.pid(aois, year, pid, True))
     get_requests.rcbl(parcel, start_date, end_date, [band],
                       chipsize, images_dir)
 
--- cbm/show/background.py
+++ cbm/show/background.py
@@ -7,17 +7,17 @@
 # Copyright : 2021 European Commission, Joint Research Centre
 # License   : 3-Clause BSD
 
-from os.path import join, normpath, isfile
-from copy import copy
-from descartes import PolygonPatch
-from rasterio.plot import show
+import json
 import rasterio
 import matplotlib.pyplot as plt
-import json
+from copy import copy
+from os.path import join, normpath, isfile
+from descartes import PolygonPatch
+from rasterio.plot import show
+from mpl_toolkits.axes_grid1 import ImageGrid
 
 from cbm.utils import config, spatial_utils
 from cbm.get import background as bg
-from mpl_toolkits.axes_grid1 import ImageGrid
 
 
 def overlay_parcel(img, geom):
@@ -28,7 +28,89 @@ def overlay_parcel(img, geom):
     return patche
 
 
-def main(aoi, year, pid, chipsize=512, extend=512, tms=['Google']):
+def by_location(aoi, year, lon, lat, chipsize=512, extend=512, tms=['Google'],
+                columns=4, quit=True):
+    """Show the background image with parcels polygon overlay by selected
+    parcel id. This function will get an image from the center of the polygon.
+
+    Examples:
+        from cbm.view import background
+        background.by_location(aoi, year, lon, lat, 512, 512, 'Google',
+                                True, True)
+
+    Arguments:
+        aoi, the area of interest e.g.: es, nld (str)
+        year, the year of the parcels dataset (int)
+        lon, lat, longitude and latitude in decimal degrees (float).
+        chipsize, size of the chip in pixels (int).
+        extend, size of the chip in meters  (float).
+        tms, tile map server Google or Bing (str).
+        quiet, print or not procedure information (Boolean).
+    """
+    show_parcel = True
+
+    if type(tms) is str:
+        tms = [tms]
+
+    try:
+        from cbm.sources import api
+        json_data = json.loads(api.ploc(aoi, year, lon, lat, True))
+        if type(json_data['ogc_fid']) is list:
+            pid = json_data['ogc_fid'][0]
+        else:
+            pid = json_data['ogc_fid']
+
+        workdir = normpath(join(config.get_value(['paths', 'temp']),
+                                f'{aoi}{str(year)}', str(pid)))
+    except Exception:
+        workdir = normpath(join(config.get_value(['paths', 'temp']),
+                                f'{aoi}{str(year)}', f'_{lon}_{lat}'))
+        show_parcel = False
+        if not quit:
+            print("No parcel information found.")
+
+    if len(tms) < columns:
+        columns = len(tms)
+
+    bg_path = normpath(join(workdir, 'backgrounds'))
+
+    for t in tms:
+        if not isfile(normpath(join(bg_path, f'{t.lower()}.tif'))):
+            bg.by_location(aoi, year, lon, lat, chipsize, extend, t, True)
+
+    if show_parcel:
+        with open(normpath(join(workdir, 'info.json')), 'r') as f:
+            json_data = json.load(f)
+
+        with rasterio.open(normpath(join(bg_path, f'{tms[0].lower()}.tif'))) as img:
+            img_epsg = img.crs.to_epsg()
+            geom = spatial_utils.transform_geometry(json_data, img_epsg)
+            patches = overlay_parcel(img, geom)
+
+    rows = int(len(tms) // columns + (len(tms) % columns > 0))
+    fig = plt.figure(figsize=(30, 10 * rows))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                     nrows_ncols=(rows, columns),  # creates grid of axes
+                     axes_pad=0.4,  # pad between axes in inch.
+                     )
+
+    for ax, t in zip(grid, tms):
+        with rasterio.open(normpath(join(bg_path, f'{t.lower()}.tif'))) as img:
+            if show_parcel:
+                for patch in patches:
+                    ax.add_patch(copy(patch))
+            show(img, ax=ax)
+            ax.set_title(t, fontsize=20)
+
+    if len(tms) > columns:
+        for ax in grid[-((columns * rows - len(tms))):]:
+            ax.remove()
+
+    plt.show()
+
+
+def by_pid(aoi, year, pid, chipsize=512, extend=512, tms=['Google'],
+           columns=4, quit=True):
     """Show the background image with parcels polygon overlay by selected
     parcel id. This function will get an image from the center of the polygon.
 
@@ -47,7 +129,8 @@ def main(aoi, year, pid, chipsize=512, extend=512, tms=['Google']):
         quiet, print or not procedure information (Boolean).
     """
 
-    columns = 4
+    if type(tms) is str:
+        tms = [tms]
     if len(tms) < columns:
         columns = len(tms)
 
