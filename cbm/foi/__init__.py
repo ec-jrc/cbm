7925ef5339f5e7870f4a9a4da3be1458ae168386
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-07-23 17:14:48 +0000

    Add import_db_functions for FOI main module

--- cbm/foi/__init__.py
+++ cbm/foi/__init__.py
@@ -6,3 +6,22 @@ def v1(*args):
 def v2(*args):
     from cbm.foi import foi_v2
     return foi_v2.main(*args)
+
+
+def import_db_functions():
+    from os.path import dirname, abspath, join, normpath, basename
+    from cbm.datas import db
+    from cbm.utils import config
+    import glob
+
+    path_foi_func = normpath(join(dirname(abspath(__file__)), 'foi_db_func'))
+    functions = glob.glob(f"{path_foi_func}/*.func")
+    db_conn = config.get_value(['set', 'db_conn'])
+    sche = config.get_value(['db', db_conn, 'sche'])
+    user = config.get_value(['db', db_conn, 'user'])
+    for f in functions:
+        db.insert_function(open(f).read().format(schema=sche, owner=user))
+        if db.db_func_exist(basename(f).split('.')[0]):
+            print(f"The '{basename(f)}' Was imported to the database.")
+        else:
+            print(f"Could not add function '{basename(f)}' to dattabase.")
