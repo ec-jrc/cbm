500d748c8f82c46077e61bf27c89de5aaa7f269e
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-09-17 14:58:28 +0000

    Version update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cbm',
-    version='0.0.6',
+    version='0.0.8',
     python_requires='>=3.6',
     description='Checks by Monitoring (CbM)',
     long_description=long_description,
