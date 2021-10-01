d6518cb6487fe72e9677de29f90f6b2c29b8ff94
Konstantinos ANASTASAKIS <Konstantinos.ANASTASAKIS@ext.ec.europa.eu>
2021-10-01 13:01:28 +0000

    Update init module

--- cbm/utils/init.py
+++ cbm/utils/init.py
@@ -15,31 +15,33 @@ from cbm.utils import config, update
 from pkg_resources import get_distribution, DistributionNotFound
 
 
-if sys.version_info < (3, 6):
-    print("Not supoted python version, cbm needs python version > 3.6")
-    sys.exit()
+def main():
+    global __version__
+    if sys.version_info < (3, 6):
+        print("Not supoted python version, cbm needs python version > 3.6")
+        sys.exit()
 
-try:
-    _dist = get_distribution('cbm')
-    # Normalize case for Windows systems
-    dist_loc = normcase(_dist.location)
-    here = normcase(__file__)
-    if not here.startswith(normpath(join(dist_loc, 'cbm'))):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version
-
-paths = config.get_value(['paths'])
-for p in paths:
     try:
-        if not exists(normpath(join(config.path_work, paths[p]))):
-            os.makedirs(normpath(join(config.path_work, paths[p])))
-            print(f"Directory {p} created successfully")
-    except OSError as err:
-        print(f"Directory {p} can not be created: ", err)
+        _dist = get_distribution('cbm')
+        # Normalize case for Windows systems
+        dist_loc = normcase(_dist.location)
+        here = normcase(__file__)
+        if not here.startswith(normpath(join(dist_loc, 'cbm'))):
+            # not installed, but there is another version that *is*
+            raise DistributionNotFound
+    except DistributionNotFound:
+        __version__ = 'Please install this project with setup.py'
+    else:
+        __version__ = _dist.version
 
-config.create()
-update.check()
+    paths = config.get_value(['paths'])
+    for p in paths:
+        try:
+            if not exists(normpath(join(config.path_work, paths[p]))):
+                os.makedirs(normpath(join(config.path_work, paths[p])))
+                print(f"The folder {p} is created successfully.")
+        except OSError as err:
+            print(f"The folder {p} can not be created: ", err)
+
+    config.create()
+    update.check()
