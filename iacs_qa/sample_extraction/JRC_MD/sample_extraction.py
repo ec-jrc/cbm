9780d71dff018ea3b1e1507319ee29de27c3fe59
mdobrychlop <mateusz.dobrychlop@gmail.com>
2024-04-25 22:24:20 +0200

    Add actual calculations

---
+++ iacs_qa/sample_extraction/JRC_MD/sample_extraction.py
@@ -0,0 +1,251 @@
+import pandas as pd
+import datetime
+import cosmetics_tools
+
+def extract_interventions(path):
+    """
+    Extracts the interventions from the csv file and returns a dataframe with the columns:
+    - gsa_par_id
+    - gsa_hol_id
+    - intervention_type_id
+    - ranking
+    """
+    interventions_full_df = pd.read_csv(path)
+    # filter donw to take only rows where covered = 1
+    interventions_full_df = interventions_full_df[interventions_full_df["covered"] == 1]
+    parcel_df = interventions_full_df[["gsa_par_id", "gsa_hol_id", "ua_grp_id", "ranking"]]
+    parcel_df = parcel_df.sort_values(by="ranking")
+    parcel_df = parcel_df.rename(columns={"ua_grp_id": "intervention_type_id", 
+                                                        "gsa_hol_id": "gsa_hol_id", 
+                                                        "gsa_par_id": "gsa_par_id"})
+    
+    # add a unique row id column that is combination of gsa_par_id, gsa_hol_id and intervention_type_id
+    # this will be used to identify which rows were already added to buckets
+    parcel_df['row_id'] = parcel_df['gsa_par_id'].astype(str) + parcel_df['gsa_hol_id'].astype(str) + parcel_df['intervention_type_id'].astype(str)
+    parcel_df['order_added'] = 0
+
+
+    return parcel_df
+
+def create_ua_grp_id_map(buckets):
+    """
+    
+    """
+    mapping = {}
+    counter = 0
+    for key in buckets:
+        mapping[key] = counter
+        counter += 1
+
+    return mapping
+
+def translate_buckets(buckets, map):
+    new_buckets = {}
+    for key in buckets:
+        new_key = map[key] # does this change keys in the original dictionary? answer: no, to do so you must reassign the value to the original dictionary:
+        new_buckets[new_key] = buckets[key]
+    return new_buckets
+
+def translate_parcel_df(parcel_df, map):
+    parcel_df["ua_grp_id"] = parcel_df["ua_grp_id"].apply(lambda x: map[x])
+    return parcel_df
+
+
+# def prepare_input_dataframe(parcel_df):
+#     parcel_df = parcel_df.sort_values(by="ranking")
+#     parcel_df["row_id"] = F"{parcel_df["gsa_par_id"].astype(str)}_{parcel_df["gsa_hol_id"].astype(str)}_{parcel_df["ua_grp_id"].astype(str)}"
+#     parcel_df["order_added"] = 0
+#     return parcel_df
+
+def prepare_input_dataframe(parcel_df):
+    parcel_df = parcel_df[parcel_df["covered"] == 1]
+    # Sort the dataframe by the 'ranking' column
+    parcel_df = parcel_df.sort_values(by="ranking")
+
+    # Create a 'row_id' by concatenating the string representations of several columns row-wise
+    parcel_df["row_id"] = parcel_df.apply(lambda row: f"{row['gsa_par_id']}_{row['gsa_hol_id']}_{row['ua_grp_id']}", axis=1)
+
+    # Initialize 'order_added' column to 0
+    parcel_df["order_added"] = 0
+
+    return parcel_df
+
+def buckets_global_count(buckets):
+    """
+    Returns a number of rows already added to all buckets together
+    """
+    return sum([len(bucket['parcels']) for bucket in buckets.values()])
+
+def extract_buckets(path):
+    """
+    Extracts the targets from the csv file and returns a dictionary with the keys being the intervention_type_id
+    and the values being a dictionary with the keys:
+    - target: the target number of parcels
+    - parcels: a list of dictionaries with the keys:
+        - gsa_par_id
+        - gsa_hol_id
+        - ranking
+    """
+    targets_full_df = pd.read_csv(path)
+    targets_df = targets_full_df[["ua_grp_id", "target1"]]
+    targets = targets_df.set_index('ua_grp_id').T.to_dict('records')[0]
+    buckets = {}
+    for id, target in targets.items():
+        if target > 300:
+            target = 300
+        buckets[id] = {'target': target, 'parcels': []}
+    return buckets
+
+def prepare_buckets(ua_groups_dict):
+    # PARAMETERS["ua_groups"][group] = {"target" : 300, "count" : ua_group_count[group]}
+    buckets = {}
+    for group_id, info in ua_groups_dict.items():
+        target = info["target"]
+        buckets[group_id] = {'target': target, 'parcels': []}
+    return buckets
+
+def generate_output(buckets):
+    """
+    Generates an output xlsx file with the following columns:
+    - bucket_id
+    - gsa_par_id
+    - gsa_hol_id
+    - ranking
+    - target
+    """
+    output = []
+    for bucket_id, bucket in buckets.items():
+        for parcel in bucket['parcels']:
+            output.append([bucket_id, parcel["gsa_par_id"], parcel["gsa_hol_id"], parcel["ranking"], parcel["order_added"], bucket['target']])
+    output_df = pd.DataFrame(output, columns=["bucket_id", "gsa_par_id", "gsa_hol_id", "ranking", "order_added", "target"])
+
+    filename = "output/NEW_output_" + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".xlsx"
+    output_df.to_excel(filename, index=False)
+
+# -----
+
+
+
+
+def buckets_full(buckets):
+    """
+    Returns True if all buckets are full, False otherwise
+    """
+    return all(len(bucket['parcels']) >= bucket['target'] for bucket in buckets.values())
+
+
+def check_holding_group_old(holding_group, buckets, added_rows):
+    """
+    Checks the holding group for parcels that can be added to buckets.
+    If possible, adds up to 3 parcels from the holding group to buckets.
+    Adds added rows to the added_rows set, and the holding group to the checked_holdings set.
+    """
+    counter = 3
+    for index, holding_row in holding_group.iterrows():
+        if buckets_full(buckets) or counter == 0:
+            break
+        for bucket_id, bucket in buckets.items():
+            if holding_row["ua_grp_id"] == bucket_id and len(bucket['parcels']) < bucket['target'] and holding_row["row_id"] not in added_rows:
+                bucket['parcels'].append({"gsa_par_id": holding_row["gsa_par_id"],
+                                          "gsa_hol_id": holding_row["gsa_hol_id"],
+                                          "ranking": holding_row["ranking"],
+                                          
+                                          })
+                added_rows.add(holding_row["row_id"])
+                counter -= 1
+
+    return buckets
+
+def all_buckets_used_3_times(bucket_counter):
+    """
+    Returns True if all buckets have been used 3 times, False otherwise.
+    """
+    return all(value == 3 for value in bucket_counter.values())
+
+def check_holding_group(holding_group, buckets, added_rows):
+    """
+    New version that adds parcels the right (?) way.
+    """
+    # create a dictionary that has the same keys as buckets, but the values are just zeros
+    bucket_counter = {key: 0 for key in buckets.keys()}
+    for index, holding_row in holding_group.iterrows():
+        if buckets_full(buckets) or all_buckets_used_3_times(bucket_counter):
+            break
+        parcel_group = holding_group[holding_group["gsa_par_id"] == holding_row["gsa_par_id"]]
+        buckets, bucket_counter = check_parcel(parcel_group, buckets, added_rows, bucket_counter)
+
+    return buckets
+
+
+def check_parcel(parcel_group, buckets, added_rows, bucket_counter=None):
+    for index, parcel_row in parcel_group.iterrows():
+        if buckets_full(buckets):
+            break
+        for bucket_id, bucket in buckets.items():
+                if parcel_row["ua_grp_id"] == bucket_id and len(bucket['parcels']) < bucket['target'] and parcel_row["row_id"] not in added_rows:
+                    if bucket_counter == None or bucket_counter[bucket_id] < 3:
+                        bucket['parcels'].append({"gsa_par_id": parcel_row["gsa_par_id"],
+                                                "gsa_hol_id": parcel_row["gsa_hol_id"],
+                                                "ranking": parcel_row["ranking"],
+                                                "order_added" : buckets_global_count(buckets)+1,
+                                                })
+                        added_rows.add(parcel_row["row_id"])
+                        if bucket_counter != None:
+                            bucket_counter[bucket_id] += 1
+    
+    return buckets, bucket_counter
+
+
+def iterate_over_interventions(parcel_df, buckets): #, progress_bars):
+    """
+    Main loop of the script.
+    Iterates over the rows in the interventions dataframe and adds parcels to the buckets.
+    """
+    stop_flag = False
+
+    print("Buckets: (\033[92mgreen\033[0m = full, \033[93myellow\033[0m = still looking for parcels)")
+
+    checked_holdings = set()
+    added_rows = set()
+    for index, row in parcel_df.iterrows():
+        
+        if buckets_full(buckets):
+            break
+        if row["gsa_hol_id"] not in checked_holdings:
+            checked_holdings.add(row["gsa_hol_id"])
+            holding_group = parcel_df[parcel_df["gsa_hol_id"] == row["gsa_hol_id"]]
+            buckets = check_holding_group(holding_group, buckets, added_rows)
+        else:
+            #buckets = check_individual_row(row, buckets, added_rows)
+            parcel_group = parcel_df[parcel_df["gsa_par_id"] == row["gsa_par_id"]]
+            buckets, bucket_counter = check_parcel(parcel_group, buckets, added_rows)
+
+        cosmetics_tools.print_progress(buckets)
+
+        # print(dir(cosmetics_tools))
+        # exit()
+        #cosmetics_tools.update_progress_bars(buckets, progress_bars)
+
+    return buckets
+
+
+#---
+
+
+# interventions_path = "input/MT_ua_grp_tiles.csv"
+# targets_path = "input/MT_view_target_sample_size.csv"
+
+# parcel_df = extract_interventions(interventions_path)
+# buckets = extract_buckets(targets_path)
+# # pbars = cosmetics_tools.show_progress_bars(buckets)
+
+# iterate_over_interventions(parcel_df, buckets) #, pbars)    
+
+# # Indicate the reason why the run ended
+# if buckets_full(buckets):
+#     print("\nAll buckets full!")
+# else:
+#     print("\nSome buckets not full!")
+
+# generate_output(buckets)   
+# print("\nOutput file generated.")
--- iacs_qa/sample_extraction/JRC_MD/sample_extraction_gui.py
+++ iacs_qa/sample_extraction/JRC_MD/sample_extraction_gui.py
@@ -11,10 +11,14 @@ import pandas as pd
 # - indicator if value loaded, manually modified, or limited by threshold
 
 
+# Global parameters for the GUI
+# This definitely needs to be refactored into a class
 PARAMETERS = {"parcels_path": "",
               "targets_path": "",
-              "ua_groups_and_thresholds": {},
+              "ua_groups": {},
               "parcel_file_loaded":False,
+              "target_values_state": "(Target values loaded from the parcel file.)",
+              "parcels_df": None,
               }
 
 style_html = """
@@ -33,7 +37,17 @@ display(HTML(style_html))
 
 
 def create_text_entry(description, placeholder, width='500px'):
-    """Create a text entry widget with specified properties."""
+    """
+    Create a text entry widget using the ipywidgets library with customizable description, placeholder, and width.
+
+    Args:
+    description (str): Label displayed next to the text box explaining its use.
+    placeholder (str): Placeholder text displayed inside the text box when empty.
+    width (str, optional): Width of the text box, specified as a CSS-like string (e.g., '500px'). Defaults to '500px'.
+
+    Returns:
+    ipywidgets.Combobox: Configured text entry widget.
+    """
     return widgets.Combobox(
         placeholder=placeholder,
         description=description,
@@ -43,8 +57,42 @@ def create_text_entry(description, placeholder, width='500px'):
         layout=widgets.Layout(width=width)
     )
 
+def create_int_entry(description, placeholder, min, max, width='500px'):
+    """
+    Create an integer entry widget with specified range and display properties.
+
+    Args:
+    description (str): Text describing the widget.
+    placeholder (str): Placeholder text displayed in the widget.
+    min (int): Minimum value the widget allows.
+    max (int): Maximum value the widget allows.
+    width (str, optional): Width of the widget, specified in CSS units. Defaults to '500px'.
+
+    Returns:
+    ipywidgets.BoundedIntText: Configured bounded integer entry widget.
+    """
+    return widgets.BoundedIntText(
+        placeholder=placeholder,
+        description=description,
+        min=min,
+        max=max,
+        style={"description_width": "initial"},
+        layout=widgets.Layout(width=width)
+    )
+
 def create_button(description, button_style, tooltip, color='#1daee3'):
-    """Create a button with specified properties."""
+    """
+    Create a button widget with customizable styling options.
+
+    Args:
+    description (str): Text displayed on the button.
+    button_style (str): Style of the button (e.g., 'success', 'info', 'warning', 'danger').
+    tooltip (str): Tooltip text that appears when the user hovers over the button.
+    color (str, optional): Background color of the button, specified as a hex code. Defaults to '#1daee3'.
+
+    Returns:
+    ipywidgets.Button: Configured button widget.
+    """
     return widgets.Button(
         description=description,
         button_style=button_style,
@@ -52,26 +100,111 @@ def create_button(description, button_style, tooltip, color='#1daee3'):
         style={"button_color": color}
     )
 
-def create_info_button(info):
-    """Create an information button with predefined style and tooltip."""
-    return widgets.Button(
+def create_info_button(tooltip, css_class="info-button-style"):
+    """
+    Create an information button with a predefined style and a tooltip.
+
+    Args:
+    tooltip (str): Text displayed as a tooltip when the user hovers over the button.
+    css_class (str, optional): CSS class to be applied for styling. Defaults to "info-button-style".
+
+    Returns:
+    ipywidgets.Button: Styled button with an information icon.
+    """
+    button = widgets.Button(
         icon="info",
         layout=widgets.Layout(width="30px"),
         button_style='',
-        tooltip="Detailed info about file loading and verification."
+        tooltip=tooltip
     )
+    button.add_class(css_class)
+    return button
+
+
+def target_widgets_on_value_change(change):
+    PARAMETERS["target_values_state"] = f"(Target values were manually modified. Last change: {change['owner'].description} field changed to {change["new"]}.)"
+    PARAMETERS["target_source_label"].value = PARAMETERS["target_values_state"]
+
+def get_target_values_from_widgets(widgets_list):
+    for widget_box in widgets_list:
+        widget = widget_box.children[0]
+        PARAMETERS["ua_groups"][widget.description]["target"] = widget.value
+
+def create_target_widgets():
+    widget_list = []
+    for group, info in PARAMETERS["ua_groups"].items():
+        entry = widgets.BoundedIntText(
+            value=info["target"],
+            min=0,
+            max=int(1e10),
+            description=group,
+            layout=widgets.Layout(width="150px"),
+            style={"description_width": "initial"}
+            
+        )
+        entry.observe(target_widgets_on_value_change, names='value')
+
+        count_label = widgets.Label(value=f"({info["count"]} found)")
+
+        group_box = widgets.HBox([entry, count_label])
+        widget_list.append(group_box)
+    return widget_list
 
 def get_ua_groups_from_parcel_file(parcels_df):
-    """Extract unique ua_grp_id values from the parcel dataframe.
-    Then populate the ua_groups_and_thresholds dictionary with these ids.
-    Set default threshold values to 300 for each group.
+    """
+    Extract unique ua group identifiers from a parcel dataframe and update a global dictionary with default thresholds.
+
+    Args:
+    parcels_df (pandas.DataFrame): Dataframe containing parcel data with a column "ua_grp_id".
     """
     ua_groups = parcels_df["ua_grp_id"].unique()
+    ua_group_count = parcels_df["ua_grp_id"].value_counts().to_dict()
     for group in ua_groups:
-        PARAMETERS["ua_groups_and_thresholds"][group] = 300
+        PARAMETERS["ua_groups"][group] = {"target" : 300, "count" : ua_group_count[group]}
+
+
+def populate_target_values(target_df, widgets_list):
+    """
+    Populate the values of target widgets using a dataframe that contains target values.
+
+    Args:
+    target_df (pandas.DataFrame): Dataframe containing the target values with columns "ua_grp_id" and "target".
+    widgets_list (list): List of widget objects where each widget's description matches a "ua_grp_id".
+    """
+    for index, row in target_df.iterrows():
+        ua_group = row["ua_grp_id"]
+        target = row["target"]
+        for widget_box in widgets_list:
+            widget = widget_box.children[0]
+            if widget.description == ua_group:
+                widget.value = target
+                break
+
+def recalculate_targets_based_on_threshold(entry_widget, widgets_list):
+    """
+    Adjust the values of target fields based on a new threshold set by the user.
+
+    Args:
+    entry_widget (ipywidgets.Widget): Widget that contains the new threshold value.
+    widgets_list (list): List of target widgets to be adjusted.
+    """
+    threshold = entry_widget.value
+    for widget_box in widgets_list:
+        widget = widget_box.children[0]
+        if widget.value > threshold:
+            widget.value = threshold
+    PARAMETERS["target_values_state"] = "(Target values modified using the target cutoff setting.)"
+    PARAMETERS["target_source_label"].value = PARAMETERS["target_values_state"]
+    get_target_values_from_widgets(widgets_list)
 
 def load_parcel_file(entry_widget, output_area):
-    """Callback function to load a file and display its contents."""
+    """
+    Load parcel data from a file specified by the user, verify its structure, and update the UI accordingly.
+
+    Args:
+    entry_widget (ipywidgets.Text): Widget containing the file path to the parcel file.
+    output_area (ipywidgets.Output): Widget used to display messages and file content.
+    """
     with output_area:
         clear_output()
         print("Verifying file. Please wait...\n")
@@ -84,17 +217,48 @@ def load_parcel_file(entry_widget, output_area):
             get_ua_groups_from_parcel_file(df)
             print("File loaded successfully. Preview: \n")
             print(df.head())
+            PARAMETERS["parcels_df"] = df
             PARAMETERS["parcel_file_loaded"] = True
         except Exception as e:
             clear_output()
             print(f"Error loading file: {e}\n")
 
+def load_target_file(b, entry_widget, output_area, widgets_list):
+    """
+    Load target values from a specified file, populate widgets, and handle errors or discrepancies in the data.
+
+    Args:
+    b (ipywidgets.Button): Button that triggered the file load.
+    entry_widget (ipywidgets.Text): Widget containing the file path to the target file.
+    output_area (ipywidgets.Output): Widget used to display messages and data.
+    widgets_list (list): List of widgets that will be populated with the target values.
+    """
+    file_path = entry_widget.value
+    with output_area:
+        clear_output()
+        try:
+            df = pd.read_csv(file_path)
+            verify_target_df(df)
+            populate_target_values(df, widgets_list)
+            compare_bucket_lists(PARAMETERS["ua_groups"], df)
+            PARAMETERS["target_values_state"] = "(Target values loaded from the targets file.)"
+            PARAMETERS["target_source_label"].value = PARAMETERS["target_values_state"]
+            get_target_values_from_widgets(widgets_list)
+            print("Target file loaded successfully. Fields repopulated. \n")
+        except Exception as e:
+            clear_output()
+            print(f"Error loading file: {e}\n")
+
+
 
 def display_parcel_input_config():
-    """Display the widgets for configuring the input parcel file path."""
+    """
+    Display a user interface for configuring the parcel file input path and load button.
+    """
     parcel_file_path_entry = create_text_entry("Parcel file path:", "example: input/parcels.csv")
     parcel_file_load_button = create_button("Load", "info", "Click to load parcel file")
     parcel_info_button = create_info_button("Info text here.")
+    parcel_info_button.add_class("info-button-style")
     
     output_area = widgets.Output()
     parcel_file_load_button.unobserve_all()
@@ -104,83 +268,43 @@ def display_parcel_input_config():
     vbox = widgets.VBox([hbox, output_area])
     display(vbox)
 
-def create_info_button(tooltip, css_class="info-button-style"):
-    """Create an information button with predefined style and tooltip."""
-    button = widgets.Button(
-        icon="info",
-        layout=widgets.Layout(width="30px"),
-        button_style='',
-        tooltip=tooltip
-    )
-    button.add_class(css_class)
-    return button
-
-def create_target_widgets():
-    """Create target widgets based on the parameters dictionary."""
-    widgets_list = []
-    if PARAMETERS["ua_groups_and_thresholds"] == {}:
-        print("No ua_grp_id values found in the parcel data. Please load the parcel file first.")
-    for group, threshold in PARAMETERS["ua_groups_and_thresholds"].items():
-        label = group
-        entry = widgets.BoundedIntText(
-            value=threshold, 
-            min=0, 
-            max=int(1e10), 
-            description=label, 
-            layout=widgets.Layout(width="150px"),
-            style={"description_width": "initial"}
-        )
-        widgets_list.append(entry)
-    return widgets_list
-
-def populate_target_values(target_df, widgets_list):
-    """Populate widget values from the target dataframe."""
-    for index, row in target_df.iterrows():
-        ua_group = row["ua_grp_id"]
-        target = row["target"]
-        for widget in widgets_list:
-            if widget.description == ua_group:
-                widget.value = target
-                break
-
-def load_target_file(b, entry_widget, output_area, widgets_list):
-    """Load target values from a file and populate widgets."""
-    file_path = entry_widget.value
-    with output_area:
-        clear_output()
-        print("Verifying file. Please wait...\n")
-        try:
-            df = pd.read_csv(file_path)
-            verify_target_df(df)
-            populate_target_values(df, widgets_list)
-            compare_bucket_lists(PARAMETERS["ua_groups_and_thresholds"], df)
-            print("File loaded successfully. Fields populated. \n")
-            print(df.head())
-        except Exception as e:
-            clear_output()
-            print(f"Error loading file: {e}\n")
-
 
 def display_bucket_targets_config():
-    """Set up and display the bucket targets configuration interface."""
-    #ua_groups = { "YFS": 1, "SFS": 2, "ME2": 3, "ES2": 4, "ES1": 5, "E7B": 6, "E7A": 7, "E5": 8, "E4": 9, "E3": 10, "E2": 11, "E1C": 12, "CIS: LA": 13, "???" : 14, "C6C": 15, "C5": 16, "C4": 17, "C3": 18, "C2B": 19, "C1": 20, "BIS": 21, "ANC": 22}
+    """
+    Set up and display a user interface for configuring and populating target values for different buckets using widgets.
+    """
     widgets_list = create_target_widgets()
 
-    target_info_button = create_info_button("You can either provide the target values manually using the fields below, or load the values from a CSV file.")
-    target_file_path_entry = create_text_entry("Bucket targets file path:", "example: input/targets.csv")
-    target_file_load_button = create_button("Load", "info", "Click to pre-populate target values")
+    if widgets_list == []:
+        print("Please load the parcel file first and run this cell again.")
+    else:
+        target_source_label = count_label = widgets.Label(value=PARAMETERS["target_values_state"])
+        PARAMETERS["target_source_label"] = target_source_label
 
-    output_area = widgets.Output()
-    target_file_load_button.on_click(lambda b: load_target_file(b, target_file_path_entry, output_area, widgets_list))
+        target_info_button = create_info_button("You can either provide the target values manually using the fields below, or load the values from a CSV file.")
+        target_info_button.add_class("info-button-style")
+        target_file_path_entry = create_text_entry("Bucket targets file path:", "example: input/targets.csv")
+        target_file_load_button = create_button("Load", "info", "Click to pre-populate target values")
+        
+        output_area = widgets.Output()
+        target_file_load_button.on_click(lambda b: load_target_file(b, target_file_path_entry, output_area, widgets_list))
 
-    grid = widgets.GridBox(widgets_list, layout=widgets.Layout(grid_template_columns="repeat(4, 200px)"))
-    hbox = widgets.HBox([target_info_button, target_file_path_entry, target_file_load_button], layout=widgets.Layout(padding="10px"))
-    vbox = widgets.VBox([hbox, output_area, grid])
-    display(vbox)
+        target_cutoff_info_button = create_info_button("Info text here.")
+        target_cutoff_info_button.add_class("info-button-style")
+        target_cutoff_entry = create_int_entry("Target cutoff:", "example: 300", 0, int(1e10), width="150px")
+        target_cutoff_recalculate_button = create_button("Recalculate", "info", "Click to recalculate target values")
+        target_cutoff_recalculate_button.on_click(lambda b: recalculate_targets_based_on_threshold(target_cutoff_entry, widgets_list))
+
+        hbox0 = widgets.HBox([target_source_label])
+        grid = widgets.GridBox(widgets_list, layout=widgets.Layout(grid_template_columns="repeat(3, 300px)", padding="10px"))
+        hbox1 = widgets.HBox([target_file_path_entry, target_file_load_button,target_info_button], layout=widgets.Layout(padding="10px"))
+        hbox2 = widgets.HBox([target_cutoff_entry, target_cutoff_recalculate_button, target_cutoff_info_button], layout=widgets.Layout(padding="10px"))
+        vbox = widgets.VBox([hbox0, grid, hbox1, hbox2, output_area])
+        display(vbox)
 
 def display_advanced_config():
     """
-    Display the widgets for configuring advanced parameters.
+    Display a configuration interface with advanced options for setting constraints on parcel processing and output settings.
     """
    
     # Widgets for holding target
@@ -216,4 +340,10 @@ def display_advanced_config():
     
     # Display all settings together
     config_box = widgets.VBox([holding_box, holding_percentage_box, image_coverage_box, output_box])
+    
+    # make all widgets in the config_box boxes disabled:
+    for box in config_box.children:
+        for widget in box.children:
+            widget.disabled = True
+    
     display(config_box)
